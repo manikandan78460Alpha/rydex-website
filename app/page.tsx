d8061e65ea4b058343d307c4bb89d353ce2d0689
Manikandan R <manikandan78460@gmail.com>
2026-06-21 08:02:08 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,19 @@ import Hero from "./components/Hero";
 import Problem from "./components/Problem";
 import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
+import PageBackground from "./components/PageBackground";
 
 export default function Home() {
   return (
-    <main>
-      <Navbar />
-      <Hero />
-      <Problem />
-      <Features />
-      <HowItWorks />
+    <main style={{ position: "relative" }}>
+      <PageBackground />
+      <div style={{ position: "relative", zIndex: 1 }}>
+        <Navbar />
+        <Hero />
+        <Problem />
+        <Features />
+        <HowItWorks />
+      </div>
     </main>
   );
 }
