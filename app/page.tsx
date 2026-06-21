0451729b45724284ac388cfc9b7b47264f48eb33
Manikandan R <manikandan78460@gmail.com>
2026-06-21 07:19:44 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Problem from "./components/Problem";
 import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Problem />
       <Features />
+      <HowItWorks />
     </main>
   );
 }
