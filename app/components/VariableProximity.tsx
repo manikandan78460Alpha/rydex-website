396ac961c2e3c2667f3c31795c884a86891e3acb
Manikandan R <manikandan78460@gmail.com>
2026-06-21 10:00:16 +0530

    Update VariableProximity.tsx

--- app/components/VariableProximity.tsx
+++ app/components/VariableProximity.tsx
@@ -1,5 +1,5 @@
 import { forwardRef, useMemo, useRef, useEffect, MutableRefObject, RefObject, HTMLAttributes } from 'react';
-import { motion } from 'motion/react';
+import { motion } from 'framer-motion';
 import './VariableProximity.css';
 
 type Callback = () => void;
