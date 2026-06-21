66fd6383540c79c7958bac44e8160f0f0fc30b0c
Manikandan R <manikandan78460@gmail.com>
2026-06-21 06:57:40 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,23 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Problem from "./components/Problem";
 import Features from "./components/Features";
+import SplashCursor from "./components/SplashCursor";
 
 export default function Home() {
   return (
     <main>
+      <SplashCursor
+        DENSITY_DISSIPATION={3.5}
+        VELOCITY_DISSIPATION={2}
+        PRESSURE={0.1}
+        CURL={3}
+        SPLAT_RADIUS={0.2}
+        SPLAT_FORCE={6000}
+        COLOR_UPDATE_SPEED={10}
+        SHADING={true}
+        RAINBOW_MODE={false}
+        COLOR="#00D4AA"
+      />
       <Navbar />
       <Hero />
       <Problem />
